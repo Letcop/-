f8e60d2794772667137258008394d3dfabf6dba8
Jamshid91 <jamshid_tojiboyev@mail.ru>
2020-10-23 20:56:12 +0500

    added handleEvent

--- assets/js/mouseCard.js
+++ assets/js/mouseCard.js
@@ -6,6 +6,7 @@ newCardLists.forEach((hideCardList) => {
     hideCardList.lastElementChild.classList.add('hide_card_info');
     hideCardList.firstElementChild.lastElementChild.classList.add('card_date_show');
     hideCardList.offsetParent.firstElementChild.classList.add('new_card_title_hide');
+    hideCardList.offsetParent.children[1].classList.add('bgc_back');
   })
 });
 
@@ -14,5 +15,6 @@ newCardLists.forEach((showCardList) => {
     showCardList.lastElementChild.classList.remove('hide_card_info')
     showCardList.firstElementChild.lastElementChild.classList.remove('card_date_show');
     showCardList.offsetParent.firstElementChild.classList.remove('new_card_title_hide');
+    showCardList.offsetParent.children[1].classList.remove('bgc_back');
   })
 });
