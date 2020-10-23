fe4cc74db4ab9809e3780ab3006d4ec662476d1b
Jamshid91 <jamshid_tojiboyev@mail.ru>
2020-10-23 18:01:53 +0500

    added mouse Event

---
+++ assets/js/mouseCard.js
@@ -0,0 +1,18 @@
+let newCardLists = document.querySelectorAll('.new_card_list');
+
+
+newCardLists.forEach((hideCardList) => {
+  hideCardList.addEventListener('mouseover', () => {
+    hideCardList.lastElementChild.classList.add('hide_card_info');
+    hideCardList.firstElementChild.lastElementChild.classList.add('card_date_show');
+    hideCardList.offsetParent.firstElementChild.classList.add('new_card_title_hide');
+  })
+});
+
+newCardLists.forEach((showCardList) => {
+  showCardList.addEventListener('mouseout', () => {
+    showCardList.lastElementChild.classList.remove('hide_card_info')
+    showCardList.firstElementChild.lastElementChild.classList.remove('card_date_show');
+    showCardList.offsetParent.firstElementChild.classList.remove('new_card_title_hide');
+  })
+});
