9a02b64be7d954ec8be839d2ffd30c2a6c0a597b
Jamshid91 <jamshid_tojiboyev@mail.ru>
2020-10-24 15:47:23 +0500

    added new styles

--- assets/js/mouseCard.js
+++ assets/js/mouseCard.js
@@ -3,18 +3,18 @@ let newCardLists = document.querySelectorAll('.new_card_list');
 
 newCardLists.forEach((hideCardList) => {
   hideCardList.addEventListener('mouseover', () => {
-    hideCardList.lastElementChild.classList.add('hide_card_info');
-    hideCardList.firstElementChild.lastElementChild.classList.add('card_date_show');
-    hideCardList.offsetParent.firstElementChild.classList.add('new_card_title_hide');
+    hideCardList.parentElement.firstElementChild.classList.add('new_card_hide');
     hideCardList.offsetParent.children[1].classList.add('bgc_back');
+    hideCardList.children[0].children[1].classList.add('card_date_show');
+    hideCardList.children[1].classList.remove('new_card_info');
   })
 });
 
 newCardLists.forEach((showCardList) => {
   showCardList.addEventListener('mouseout', () => {
-    showCardList.lastElementChild.classList.remove('hide_card_info')
-    showCardList.firstElementChild.lastElementChild.classList.remove('card_date_show');
-    showCardList.offsetParent.firstElementChild.classList.remove('new_card_title_hide');
-    showCardList.offsetParent.children[1].classList.remove('bgc_back');
+    showCardList.parentElement.firstElementChild.classList.remove('new_card_hide');
+    showCardList.offsetParent.children[1].classList.remove('bgc_back')
+    showCardList.children[0].children[1].classList.remove('card_date_show');
+    showCardList.children[1].classList.add('new_card_info');
   })
 });
